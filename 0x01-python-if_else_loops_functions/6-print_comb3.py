8cf35d9e68423bcac15b8587b9afa722ecd9fc34
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-03-29 06:45:06 -0700

    Task_6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+for i in range(1, 100):
+    if int(str(i)[::-1]) < i:
+        continue
+    elif int(str(i)[::-1]) == i:
+        if i < 11:
+            print('{:02d}'.format(i), end= ", ")
+        else:
+            continue
+    else:
+        if i == 89:
+            print('{:02d}'.format(i))
+            break
+        print('{:02d}'.format(i), end=", ")
