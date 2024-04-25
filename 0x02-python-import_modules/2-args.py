1ffd3fd13f073b6fced279bc17c8c57493aec3e1
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-04-25 13:42:50 -0700

    Task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    length = len(argv)
+    if length == 1:
+        print(length - 1, "arguments.")
+    elif length == 2:
+        print(length - 1, "argument:")
+        print(length - 1, argv[length - 1])
+    else:
+        print(length - 1, "arguments:")
+        for i in range(length):
+            if i == 0:
+                continue
+            print(i, argv[i])
