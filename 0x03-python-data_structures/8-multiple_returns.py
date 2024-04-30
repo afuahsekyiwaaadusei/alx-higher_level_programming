743c7891df474cd0d3d23c70a30e7a868de219a1
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-04-30 07:20:29 -0700

    task 8 pyccodestyle- fixed missing whitespace after keyword return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -8,4 +8,4 @@ def multiple_returns(sentence):
     if len(sentence) > 0:
         return (len(sentence), sentence[0])
     else:
-        return(len(sentence), None)
+        return (len(sentence), None)
