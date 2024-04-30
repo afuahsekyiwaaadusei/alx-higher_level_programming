a5ae84a03466bba508f8ea7acc0a92de9ed49d65
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-04-30 02:57:54 -0700

    Task-4, account for idx negative or out of range

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    n = my_list[:]
-    n[idx] = element
-    return n
+    n = my_list.copy()
+    if idx < 0 or idx >= len(my_list):
+        return n
+    else:
+        n[idx] = element
+        return n
