6bbac14647a1a5219dc899cfa4a9044058c2897a
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-05-16 14:48:55 -0700

    Task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     add = 0
     prev = 0
-    if isinstance(roman_string, str) is False  or roman_string is None:
+    if isinstance(roman_string, str) is False or roman_string is None:
         return 0
     for i in roman_string:
         if prev < numeral[i]:
