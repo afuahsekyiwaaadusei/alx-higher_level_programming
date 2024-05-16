4d717f392c218aa93e364b721596f4205c3efd3c
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-05-16 15:32:49 -0700

    Task 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    '''Returns the weighted average of all integers tuple (<score>, <weight>).
+
+    Returns 0 if the list is empty'''
+    top = 0
+    bottom = 0
+    for x in my_list:
+        top = top + (x[0] * x[1])
+    for i in my_list:
+        bottom = bottom + i[1]
+    return top/bottom
