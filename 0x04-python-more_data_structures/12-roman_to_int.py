3eb1c063736b83ce46443595b942b7c5d91004d5
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-05-16 14:16:39 -0700

    Task 12 - account for pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,16 +5,15 @@ def roman_to_int(roman_string):
 
     def roman_to_int(roman_string) must return an integer.
     If the roman_string is not a string or None, return 0'''
-    numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numeral = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     add = 0
     prev = 0
-    if type(roman_string) != str or roman_string == None:
+    if type(roman_string) != str or roman_string is None:
         return 0
     for i in roman_string:
-        if prev < numerals[i]:
-            add = add + (numerals[i] - prev) - prev
+        if prev < numeral[i]:
+            add = add + (numeral[i] - prev) - prev
         else:
-            add = add + numerals[i]
-        prev = numerals[i]
-    
+            add = add + numeral[i]
+        prev = numeral[i]
     return add
