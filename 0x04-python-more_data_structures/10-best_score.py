b46bbfdd46fad295072abe6b196fe6c4c612143c
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmail.com>
2024-05-16 04:42:17 -0700

    Task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    '''Returns a key with the biggest integer value.
+
+    If no score found, return None.'''
+    if a_dictionary == None:
+        return None
+    big_int = 0
+    big_int_key = ''
+    for k, v in a_dictionary.items():
+        if v > big_int:
+            big_int = v
+            big_int_key = k
+    return big_int_key
