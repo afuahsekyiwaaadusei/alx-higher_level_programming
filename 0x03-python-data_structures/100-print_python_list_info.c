3b1de0fa9ef8603a94b1c204dfbc48a28dbdccb7
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmial.com>
2024-05-17 19:02:35 +0000

    Task 14

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,7 +9,16 @@ void print_python_list_info(PyObject *p)
 {
 	PyListObject *lst = (PyListObject*)(p);
 	Py_ssize_t allocated_slots = lst->allocated;
+	Py_ssize_t size = PyList_Size(p);
+	PyObject ** item = lst->ob_item;
+	struct _typeobject *type = Py_TYPE(p);
+	Py_ssize_t i;
 
-	printf("Number of allocated slots: %zd\n", allocated_slots)
-
+	printf("[*] Size of the Python List =  %zd\n", size);
+	printf("[*] Allocated = %zd\n", allocated_slots);
+	for (i = 0; i < size; i++)
+	{
+		type = Py_TYPE(item[i]);		
+		printf("Element %zd:%s\n", i, type->tp_name);
+	}
 }
