dce7c7852ab3441be24fa6ddd67aa2cc0b0dbd39
afuahsekyiwaaadusei <afuahsekyiwaaadusei@gmial.com>
2024-05-21 18:42:09 +0000

    Task 16

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -16,20 +16,20 @@ void print_python_bytes(PyObject *p)
 	if (PyBytes_Check(p))
 	{	PyBytes_AsStringAndSize(p, &buffer, &len);	
 		printf("[.] bytes object info\n");
-		printf("size: %zd\n", len);
-		printf("trying string: %s\n", buffer);
+		printf(" size: %zd\n", len);
+		printf(" trying string: %s\n", buffer);
 		obj = PyBytes_FromObject(p);
 		byte_obj = (PyBytesObject*)obj;
 		if (len <= 10)
 		{
-			printf("first %zd bytes: ", len + 1);
+			printf(" first %zd bytes: ", len + 1);
 			for(i = 0; i <= len; i++)
 				printf("%02x ", (unsigned char)byte_obj->ob_sval[i]);
 			printf("\n");
 		}
 		else
 		{
-			printf("first 10 bytes: ");
+			printf(" first 10 bytes: ");
 			for (i = 0; i < 10; i++)
 				printf("%02x ", (unsigned char)byte_obj->ob_sval[i]);
 			printf("\n");
